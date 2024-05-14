dea0ba82bfe38fae81059840759358601ddae4bc
Silveronski <153466946+Silveronski@users.noreply.github.com>
2024-05-15 02:10:17 +0300

    1

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ require('express-async-errors'); // async erros will automatically be caught and
                                  // without the need to catch the error or do 'next()'
 
 const express = require('express');
-// const mainRouter = require('./routes/mainRoute');
-// const notFound = require('./middleware/notFound');
-// const errorHandler = require('./middleware/errorHandler');
+const connectToDb = require('./db/connect');
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
+const errorHandler = require('./middleware/error-handler');
+const notFound = require('./middleware/not-found');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-// app.use('/api/v1', mainRouter);
-// app.use(errorHandler);
-// app.use(notFound);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
+app.use(errorHandler);
+app.use(notFound);
+
+connectToDb();
 
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);
---
+++ db/connect.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectToDb = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log("DB CONNECTED!", connect.connection.name);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+module.exports = connectToDb;

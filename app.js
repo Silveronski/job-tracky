4179eb41f37e652361fd3569536d223bc64559ae
Silveronski <153466946+Silveronski@users.noreply.github.com>
2024-05-18 17:47:42 +0300

    finish server

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 require('express-async-errors'); // async erros will automatically be caught and passed to the error handling middleware
                                  // without the need to catch the error or do 'next()'
 
+const cors = require('cors');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const connectToDb = require('./db/connect');
 const authRouter = require('./routes/auth');
@@ -14,6 +19,17 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(xss());
+// app.set('trust proxy', 1); uncomment if hosted on a platform that uses reverse proxy
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+}));
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 app.use(errorHandler);
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,25 @@
 const {StatusCodes} = require('http-status-codes');
-const {CustomApiError} = require('../errors');
 
 const errorHandler = (err,req,res,next) => {
-    if (err instanceof CustomApiError) {
-        return res.status(err.statusCode).json({msg: err.message});
+    let customError = {
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || 'Something went wrong...'
+    };
+
+    if (err.name === 'CastError') { // validation for bad req.params.id
+        customError.msg = `No item found with id: ${err.value}`;
+        customError.statusCode = StatusCodes.NOT_FOUND;
     }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: err});
+    if (err.name === 'ValidationError') { // validation for missing email and/or password
+        customError.msg = Object.values(err.errors).map((item) => item.message).join(',');
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+    }
+    if (err.code && err.code === 11000) { // validation for duplicate email
+        customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`;
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+    }
+
+    return res.status(customError.statusCode).json({msg: customError.msg});
 }   
 
 module.exports = errorHandler;
